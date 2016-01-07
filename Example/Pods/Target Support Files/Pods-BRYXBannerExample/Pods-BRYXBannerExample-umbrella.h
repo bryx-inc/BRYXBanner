a724b01f024fc1c93fcac07cf28122e26e1adc1c
Harlan Haskins <harlan@harlanhaskins.com>
2016-01-07 18:53:41 -0500

    Re-created example project

---
+++ Example/Pods/Target Support Files/Pods-BRYXBannerExample/Pods-BRYXBannerExample-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_BRYXBannerExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_BRYXBannerExampleVersionString[];
+
--- Example/Pods/Target Support Files/Pods-BRYXBanner_Example-BRYXBanner/Pods-BRYXBanner_Example-BRYXBanner-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BRYXBanner_Example_BRYXBanner : NSObject
-@end
-@implementation PodsDummy_Pods_BRYXBanner_Example_BRYXBanner
-@end
